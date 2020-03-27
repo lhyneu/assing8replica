5bf39b409034fcca870cc1b8a32c313aafe2dc62
Haoyu Lin <lin.haoy@husky.neu.edu>
2020-03-26 20:56:08 -0400

    add temporaray todo examples

---
+++ src/app/fake-todos.ts
@@ -0,0 +1,9 @@
+import {Todo} from "./todo-app/todo"
+
+export const TODOS: Todo[] = [
+    {id:"id1",title:"title1", description:"desc1", createdDate:new Date(), modifiedDate:new Date(), isComplete:false},
+    {id:"id2",title:"title2", description:"desc2", createdDate:new Date(), modifiedDate:new Date(), isComplete:false},
+    {id:"id3",title:"title3", description:"desc3", createdDate:new Date(), modifiedDate:new Date(), isComplete:false},
+    {id:"id4",title:"title4", description:"desc4", createdDate:new Date(), modifiedDate:new Date(), isComplete:false},
+    {id:"id5",title:"title5", description:"desc5", createdDate:new Date(), modifiedDate:new Date(), isComplete:false},
+]
