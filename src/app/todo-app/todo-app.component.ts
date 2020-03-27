b20d8d028d44c3ef059111c174f66f1f94f19211
Haoyu Lin <lin.haoy@husky.neu.edu>
2020-03-26 20:54:27 -0400

    display list of todo items

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TODOS } from '../fake-todos';
+
 @Component({
   selector: 'todo-app',
   templateUrl: './todo-app.component.html',
   styleUrls: ['./todo-app.component.scss']
 })
 export class TodoAppComponent implements OnInit {
+
+  todos = TODOS
 
   constructor() { }
 
